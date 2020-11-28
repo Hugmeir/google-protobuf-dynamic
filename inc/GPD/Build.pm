e83154bfe9d344622a2c05bafae3019afe82b99b
Brian Fraser <brian.fraser@booking.com>
2020-11-28 20:23:12 +0100

    Build against Alien::Protobuf3_1_0 and Alien::libupb_legacy
    Plus fixes to allow statically linking against those, particularly
    in boxes using musl C (e.g. alpine).

--- inc/GPD/Build.pm
+++ inc/GPD/Build.pm
@@ -2,10 +2,12 @@ package GPD::Build;
 
 use strict;
 use warnings;
+use Config;
 use parent 'Module::Build::WithXSpp';
 
-use Alien::ProtoBuf;
-use Alien::uPB;
+use Alien::Protobuf3_1_0;
+use Alien::libupb_legacy;
+use Text::ParseWords qw( shellwords );
 use Getopt::Long qw( :config pass_through );
 
 # yes, doing this in a module is ugly; OTOH it's a private module
@@ -21,10 +23,20 @@ sub new {
         extra_typemap_modules => {
             'ExtUtils::Typemaps::STL::String' => '0',
         },
-        extra_linker_flags => [Alien::uPB->libs, Alien::ProtoBuf->libs],
-        extra_compiler_flags => [$debug_flag, Alien::uPB->cflags, Alien::ProtoBuf->cflags, Alien::ProtoBuf->cxxflags, "-DPERL_NO_GET_CONTEXT"],
+        extra_linker_flags => [grep /\Q$Config{lib_ext}\E\z/, map shellwords($_), Alien::libupb_legacy->libs_static, Alien::Protobuf3_1_0->libs_static],
+        extra_compiler_flags => [$debug_flag, Alien::libupb_legacy->cflags, Alien::Protobuf3_1_0->cflags, Alien::Protobuf3_1_0->cxxflags, "-DPERL_NO_GET_CONTEXT"],
         script_files => [qw(scripts/protoc-gen-perl-gpd)],
     );
+
+    # if we ar statically linking against libprotobuf/libupb, then the
+    #   -L/dir -lprotobuf
+    # flags need to come before the objects being linked:
+    $self->config(lddlflags => join(" ", $Config{lddlflags}, Alien::libupb_legacy->libs, Alien::Protobuf3_1_0->libs_static));
+
+    # we are linking against C++ libraries (libprotobuf, upb), and possibly
+    # *statically* linking against them -- so we must use g++/clang++ etc to
+    # link, not gcc/clang, otherwise the link will fail:
+    $self->config(ld => $self->config('cc'));
 
     return $self;
 }
